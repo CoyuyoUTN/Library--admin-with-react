5abc3d66a6e1e16ef4ff643a62a0e7cbbe5c04ab
Gonzalo <herrero_gonza@hotmail.com>
2022-10-13 01:50:17 -0300

    Revert "Dev gonza"

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
-import MainPage from "./components/MainPage";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import CallJson from "./components/CallJson";
-import DetailBook from "./components/DetailBook";
-import "antd/dist/antd.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <CallJson />
-        <NavBar />
+    <div className="App">
+      
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/home" element={<MainPage />} />
-          <Route path="/register/" element={<RegisterForm />} />
-          <Route path="/login/" element={<LoginForm />} />
-          <Route path="/DetailBook/:id" element={<DetailBook />} />
-          <Route path="*" element={<MainPage />} />
+          <Route path="/" />
+          <Route path="/register/" element={<RegisterForm />} />   
+          <Route path="/login/" element={<LoginForm />} />                
+        
         </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   );
 }
 
