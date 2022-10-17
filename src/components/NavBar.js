0f357bdcb8a479ec0d30b27fb4e7feb73fcda010
CoyuyoUTN <herrero_gonza@hotmail.com>
2022-10-17 01:15:08 -0300

    new responsive nav!

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,49 +2,46 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 import Logout from "./Logout";
 
-
-
 const NavBar = () => {
   const sysInfo = useSelector((state) => state.sysConfig);
 
-
   return (
     <>
-      <Navbar bg="dark" variant="dark" fixed="top">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Library</Navbar.Brand>
-          <Nav className="me-auto">
-            <Link to={"/home"} className="nav-link">
-              Home
-            </Link>
-          </Nav>
-          <Navbar.Collapse className="justify-content-end">
-            
-              {sysInfo.logued ? 
-                (
-                <><Navbar.Text>
-                  Signed in as: {sysInfo.userName} /
-                </Navbar.Text>
-                <Nav> 
-                <Logout />
-
-                </Nav></> 
-                )
-              :
-                (<Nav> 
-                  <LoginForm />
-                  <RegisterForm />
-                </Nav> )  
-              }
-            
-
-
-        </Navbar.Collapse>          
+          <Navbar.Brand href="#home">LIBRARY!</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </Nav>
+            <Nav>
+              <Navbar.Collapse className="justify-content-end">
+                {sysInfo.logued ? (
+                  <>
+                    <Navbar.Text>
+                      Signed in as: {sysInfo.userName} /
+                    </Navbar.Text>
+                    <Nav>
+                      <Logout />
+                    </Nav>
+                  </>
+                ) : (
+                  <Nav className="gp">
+                    <LoginForm />
+                    <RegisterForm />
+                  </Nav>
+                )}
+              </Navbar.Collapse>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
